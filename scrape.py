a6f870821f05bb844dad81e5c9b513e952fd6dea
kisunji <krisna@outlook.com>
2022-07-07 14:14:50 +0545

    Modified Data

--- scrape.py
+++ scrape.py
@@ -7,38 +7,39 @@ url = 'https://bg.annapurnapost.com/api/search?title=%E0%A4%96%E0%A5%87%E0%A4%B2
 # MANUALLY ENTER THE NUMBER MORE THAN NUMBER SAVED IN PAGE.TXT FILE.
 # IF PAGE.TXT NOT EXIST THEN ENTER 0.
 
-pageno = int(input(" Enter number more than saved in page.txt "))
-
+pageno = 0
 scraped = []
 
 #  PUT THE NUMBER AS YOUR REQUIREMENT FOR SCRAPING TO CHECK THE CONDITION
 # HOW MANY PAGE YOU WANT TO SCRAPE, FOR NOW IT'S 4.
 
-while pageno <= 4:
 
+count = 1
+while count <= 3:
+
+    # CHECK WHETHER THE FILE EXIST OR NOT. IF NOT CREATE ONE.
     try:
-        res = requests.get(url+str(pageno))
-
-# CHECK WHETHER THE FILE EXIST OR NOT. IF NOT CREATE ONE.
-
         if not os.path.exists('page.txt'):
             with open('page.txt', 'w') as fp:
                 fp.write(str(pageno))
 
-# <------------------------------------------------------>
+        # <------------------------------------------------------>
 
         else:
             with open('page.txt', 'r+') as fp:
                 page_data = int(fp.read())
 
-                if pageno > page_data:
+                pageno = (1+page_data)
 
+                res = requests.get(url+str(pageno))
+
+                if pageno > page_data:
                     # SEEK HELPS TO BRING THE POINTER TO FIRST.
                     fp.seek(0)
                     fp.write(str(pageno))
 
                     # WRITE THE JSON FILE IN ORDER TO SAVE THE REQUIRED DATA.
-                    with open('jsondata.json', 'w+', encoding='utf-8') as fp:
+                    with open('jsondata.json', 'a+', encoding='utf-8') as fp:
                         for jd in res.json()['data']['items']:
                             entry_dict = {
                                 'title': jd['title'],
@@ -55,7 +56,6 @@ while pageno <= 4:
                     print("Checking Condition...")
 
     except requests.ConnectionError:
-        print('Connection Error...')
-        break
+        print("Connection Lost..")
 
-    pageno += 1
+    count += 1
